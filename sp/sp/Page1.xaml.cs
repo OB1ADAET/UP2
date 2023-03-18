6f2f52a0f9335b4648dd6f30034bea9093bba8d9
OB1ADAET <116683120+OB1ADAET@users.noreply.github.com>
2023-03-18 09:44:36 +0300

    Add files via upload

---
+++ sp/sp/Page1.xaml.cs
@@ -0,0 +1,38 @@
+﻿using sp.mysql1DataSetTableAdapters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace sp
+{
+    /// <summary>
+    /// Логика взаимодействия для Page1.xaml
+    /// </summary>
+    public partial class Page1 : Page
+    {
+        usersTableAdapter users = new usersTableAdapter();
+        public Page1()
+        {
+            InitializeComponent();
+            UsersData.ItemsSource = users.GetData();
+        }
+
+        private void ChangeBtn1_Click(object sender, RoutedEventArgs e)
+        {
+            var dataUsers = users.GetData();
+            users.InsertQuery(dataUsers[dataUsers.Count - 1].id + 1, NewUser.Text, $"{NewUser.Text}@mail.com");
+            UsersData.ItemsSource = users.GetData();
+        }
+    }
+}
---
+++ sp/sp/Page2.xaml.cs
@@ -0,0 +1,51 @@
+﻿using sp.mysql1DataSetTableAdapters;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace sp
+{
+    /// <summary>
+    /// Логика взаимодействия для Page2.xaml
+    /// </summary>
+    public partial class Page2 : Page
+    {
+        chatsTableAdapter chats = new chatsTableAdapter();
+        usersTableAdapter users = new usersTableAdapter();
+        public Page2()
+        {
+            InitializeComponent();
+            ChatsData.ItemsSource = chats.GetData();
+            user1.ItemsSource = users.GetData();
+            user1.DisplayMemberPath = "fname";
+            user2.ItemsSource = users.GetData();
+            user2.DisplayMemberPath = "fname";
+        }
+
+        private void insertBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (user1.SelectedIndex != -1 && user2.SelectedIndex != -1)
+            {
+                var newId = chats.GetData();
+                int User1 = users.GetData()[user1.SelectedIndex].id;
+
+                int User2 = users.GetData()[user2.SelectedIndex].id;
+                Debug.WriteLine(user2.SelectedIndex);
+                chats.InsertQuery(newId[newId.Count - 1].id + 1, User1, User2);
+                ChatsData.ItemsSource = chats.GetData();
+            }
+        }
+    }
+}
